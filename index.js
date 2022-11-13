3934b8a90c1082d16ef3302791a0c5fab04a0ad8
randomdiamond <97394586+randomdiamond@users.noreply.github.com>
2022-11-13 14:04:46 +0100

    add small instructions for API use

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const importData = require("./wordData.json")
 let port = process.env.PORT || 3000
 
 app.get("/", (req, res) => {
-    res.send("hello world")
+    res.send(`To get a random word just use the /randomword endpoint: ${port}/randomword`)
 })
 app.get("/randomword", (req, res) => {
     res.send(importData[Math.floor(Math.random() * importData.length)])
