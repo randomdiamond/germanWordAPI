8d6368c2883306e798e5a99303bff6ed4cfa1ab5
randomdiamond <97394586+randomdiamond@users.noreply.github.com>
2022-11-13 14:06:09 +0100

    fixed a small bug in the API instructions

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const importData = require("./wordData.json")
 let port = process.env.PORT || 3000
 
 app.get("/", (req, res) => {
-    res.send(`To get a random word just use the /randomword endpoint: ${port}/randomword`)
+    res.send(`To get a random word just use the /randomword endpoint: https://germanwordsapi.cyclic.app/randomword`)
 })
 app.get("/randomword", (req, res) => {
     res.send(importData[Math.floor(Math.random() * importData.length)])
